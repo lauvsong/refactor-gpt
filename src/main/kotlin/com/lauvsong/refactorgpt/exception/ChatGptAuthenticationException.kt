c65928412d118f7a13a6b8b2e6312309798b2e79
lauvsong <hyehye9355@naver.com>
2024-04-08 16:11:50 +0900

    ✨ Improve alert message :: authentication failed

--- src/main/kotlin/com/lauvsong/refactorgpt/exception/ChatGptAuthenticationException.kt
+++ src/main/kotlin/com/lauvsong/refactorgpt/exception/ChatGptAuthenticationException.kt
@@ -2,6 +2,10 @@ package com.lauvsong.refactorgpt.exception
 
 class ChatGptAuthenticationException(
     message: String? = """
-        Authentication failed. Check API key settings.
+        Authentication failed. Check below things.
+        1. API key settings configured in Settings.
+        2. API key settings configured in OpenAI.
+        3. Ensure a credit card is registered with OpenAI.
+        If these steps do not resolve the issue, please get a new API key and try again.
         """
 ) : Exception(message)
